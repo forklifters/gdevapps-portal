dcc8467ace9adc8fda003cb84bef59cbcc846643
Pasha Karasyuk <karac38@gmail.com>
2018-09-08 23:17:17 -0400

    added classrooms and students pages

--- GdevApps.Portal/GdevApps.Portal/Controllers/TeacherController.cs
+++ GdevApps.Portal/GdevApps.Portal/Controllers/TeacherController.cs
@@ -16,6 +16,8 @@ using System.Collections.Generic;
 using GdevApps.Portal.Models.TeacherViewModels;
 using System;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GdevApps.Portal.Controllers
 {
@@ -82,28 +84,222 @@ namespace GdevApps.Portal.Controllers
                 };
                 return Ok(new {data = classes});
             }
-            catch (Exception err)
+            catch (Exception ex)
             {
-                return BadRequest(err);
+                return BadRequest(ex);
             }
-        }
-
-
-        [HttpGet]
-        public ActionResult GetClassSheetInfo(int classroomId)
-        {
-            return PartialView("_GetClassSheetInfo", new ClassSheetsViewModel() { ClassroomId = classroomId.ToString() });
         }
 
         [HttpPost]
-        public ActionResult GetClassSheetInfo(ClassSheetsViewModel model)
+        public async Task<IActionResult> GetStudents(string classId){
+            try{
+                var allStudents = new List<StudentsViewModel>(){
+                    new StudentsViewModel{
+                        Name = "Pasha",
+                        Email = "pavlo.karasyuk@gmail.com",
+                        ClassId = "1",
+                        PrentEmails = new List<string>{"parentEmail@example.xyz","parentEmail2@example.xyz"},
+                        Id = "1-1",
+                        IsInClassroom = true
+                    },
+                     new StudentsViewModel{
+                        Name = "Sasha",
+                        Email = "Sasha@gmail.com",
+                        ClassId = "1",
+                        PrentEmails = new List<string>{"parentEmail@example.xyz","parentEmail2@example.xyz"},
+                        Id = "1101",
+                        IsInClassroom = false
+
+                    },
+                     new StudentsViewModel{
+                        Name = "Dasha",
+                        Email = "Dasha@gmail.com",
+                        ClassId = "1",
+                        PrentEmails = new List<string>{"parentEmail@example.xyz","parentEmail2@example.xyz"},
+                        Id = "1102",
+                        IsInClassroom = true
+                    },
+                     new StudentsViewModel{
+                        Name = "Alex",
+                        Email = "Alex@gmail.com",
+                        ClassId = "2",
+                        PrentEmails = new List<string>{"parentEmail@example.xyz","parentEmail2@example.xyz"},
+                        Id = "1103",
+                        IsInClassroom = false
+                    },
+                     new StudentsViewModel{
+                        Name = "Pasha",
+                        Email = "pavlo.karasyuk@gmail.com",
+                        ClassId = "3",
+                        PrentEmails = new List<string>{"parentEmail@example.xyz","parentEmail2@example.xyz"},
+                        Id = "1104",
+                        IsInClassroom = true
+                    }
+                };
+
+                if(classId == "0")
+                {
+                    return Ok(new { data = allStudents });
+                }
+
+                var filteredStudents = allStudents.Where(s => s.ClassId == classId).ToList();
+                return Ok(new {data = filteredStudents});
+            }catch(Exception ex){
+                 return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClassesForStudents()
+        {
+            try
+            {
+                List<ClassesViewModel> classes = new List<ClassesViewModel>
+                {
+                    new ClassesViewModel{
+                        CourseWorksCount = 10,
+                        Description = "This is the desciption",
+                        StudentsCount = 5,
+                        Name = "Math",
+                        Id = "2",
+                        ClassroomSheets = Singleton.Instance.Sheets.Where(s => s.ClassroomId == "2").ToList()
+                    },
+                    new ClassesViewModel{
+                        CourseWorksCount = 5,
+                        StudentsCount = 10,
+                        Name = "English",
+                        Id = "1",
+                        ClassroomSheets = Singleton.Instance.Sheets.Where(s => s.ClassroomId == "1").ToList()
+                    },
+                    new ClassesViewModel{
+                        CourseWorksCount = 5,
+                        Description = "This is the desciption for music",
+                        StudentsCount = 5,
+                        Name = "Music",
+                        Id = "3",
+                        ClassroomSheets = Singleton.Instance.Sheets.Where(s => s.ClassroomId == "3").ToList()
+                    }
+                };
+
+                var classesList = new SelectList(classes, "Id", "Name");
+
+                return View("ClassesForStudents",new ClassesForStudentsViewModel{Classes = classesList});
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddGradebook(string classroomId)
+        {
+            if(!string.IsNullOrEmpty(classroomId)){
+                return PartialView("_AddGradebook", new ClassSheetsViewModel() { ClassroomId = classroomId });
+            }
+
+            return BadRequest(classroomId);
+        }
+
+        [HttpPost]
+        public ActionResult AddGradebook(ClassSheetsViewModel model)
         {
             if (ModelState.IsValid)
             {
+                var sheet = Singleton.Instance.Sheets.Where(s => s.ClassroomId == model.ClassroomId && s.Id == model.Id).FirstOrDefault();
+                if (sheet != null)
+                {
+                    ModelState.AddModelError("Id", $"Gradebook with suck id already exists");
+                    return PartialView("_AddGradebook", model);
+                }
+
+                var isValidLink = CheckLink(model.Link);
+                if (!isValidLink)
+                {
+                    ModelState.AddModelError("Link", $"Link is not valid. Provide a valid link");
+                    return PartialView("_AddGradebook", model);
+                }
+
                 Singleton.Instance.Sheets.Add(model);
+                return PartialView("_AddGradebook", model);
             }
 
-            return PartialView("_GetClassSheetInfo", model);
+            return PartialView("_AddGradebook", model);
+        }
+
+        [HttpPost]
+        public ActionResult EditGradebook(ClassSheetsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var sheet = Singleton.Instance.Sheets.Where(s => s.Id == model.Id).FirstOrDefault();
+                if (sheet != null)
+                {
+                    var isValidLink = CheckLink(model.Link);
+                    if(!isValidLink){
+                        ModelState.AddModelError("Link", $"Link is not valid. Provide a valid link");
+                        return PartialView("_EditGradebook", model);
+                    }
+
+                    sheet.Id = model.Id;
+                    sheet.Link = model.Link;
+                    sheet.Name = model.Name;
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest($"Gradebook with id {model.Id} was not found");
+                }
+
+            }
+
+            return PartialView("_EditGradebook", model);
+        }
+
+        [HttpGet]
+        public ActionResult GetGradebookById(string classroomId, string gradebookId)
+        {
+            try
+            {
+                var sheet = Singleton.Instance.Sheets.Where(s => s.ClassroomId == classroomId && s.Id == gradebookId).FirstOrDefault();
+                if (sheet != null)
+                {
+                    return PartialView("_EditGradebook", sheet);
+                }
+                else
+                {
+                    return BadRequest($"Gradebook with id {gradebookId} was not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult RemoveGradebook(string classroomId, string gradebookId){
+            try
+            {
+                if (!string.IsNullOrEmpty(classroomId) && !string.IsNullOrEmpty(gradebookId))
+                {
+                    var sheet = Singleton.Instance.Sheets.Where(s => s.ClassroomId == classroomId && s.Id == gradebookId).FirstOrDefault();
+                    if (sheet != null)
+                    {
+                        Singleton.Instance.Sheets.Remove(sheet);
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest($"Gradebook with id {gradebookId} was not found");
+                    }
+                }
+                return BadRequest(classroomId);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
         }
 
         private async Task<List<ClassesViewModel>> GetClassesAsync()
@@ -172,6 +368,11 @@ namespace GdevApps.Portal.Controllers
             }
 
             return new List<ClassesViewModel>();
+        }
+
+        private bool CheckLink(string link)
+        {
+            return link.StartsWith("https://docs.google.com");
         }
     }
 
---
+++ GdevApps.Portal/GdevApps.Portal/Models/TeacherViewModels/StudentsViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GdevApps.Portal.Models.TeacherViewModels
+{
+    public class StudentsViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public bool IsInClassroom {get; set;}
+
+        public List<string> PrentEmails { get; set; }
+        public string ClassId { get; set; }
+    }
+}
