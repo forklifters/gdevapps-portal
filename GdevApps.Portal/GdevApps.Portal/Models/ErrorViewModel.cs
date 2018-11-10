5928f70dc5fb21fdc70df439c3fd88886e417222
Pasha Karasyuk Mac <karac38@gmail.com>
2018-11-10 17:11:13 -0500

    test commit

--- GdevApps.Portal/GdevApps.Portal/Models/ErrorViewModel.cs
+++ GdevApps.Portal/GdevApps.Portal/Models/ErrorViewModel.cs
@@ -8,7 +8,6 @@ namespace GdevApps.Portal.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
